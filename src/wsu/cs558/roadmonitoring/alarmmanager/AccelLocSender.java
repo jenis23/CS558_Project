62912ebcf5d87d6bcdb9576d3dc0e2060ee34951
Jenis Modi <jenis.a.modi@gmail.com>
2013-04-30 12:09:32 -0700

    added broadcast classes

---
+++ src/wsu/cs558/roadmonitoring/alarmmanager/AccelLocSender.java
@@ -0,0 +1,15 @@
+package wsu.cs558.roadmonitoring.alarmmanager;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AccelLocSender extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context arg0, Intent arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
